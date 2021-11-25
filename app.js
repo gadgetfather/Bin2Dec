83db3819677280733c5d4494d1e8b533bf1a0c91
Akshay Kamble <kamblea635@gmail.com>
2021-11-25 08:55:05 +0530

    added functionality

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-const input = document.querySelector('#input')
+const inputText = document.querySelector('#input')
 const output = document.querySelector('#output')
+const waitText = document.querySelector('#waiting')
+
+inputText.addEventListener('input',bin2dec)
+
+
+function bin2dec(){
+    let binValue = inputText.value
+    let decValue = parseInt(binValue,2)
+    // To check if value is NaN 
+    if(Number.isNaN(decValue)){
+        waitText.textContent = `Enter a valid number`
+        output.textContent = `Waiting for right number`
+        
+    }else{
+        output.textContent = decValue
+    }
+    console.log(parseInt(binValue,2))
+    
+    
+}
+
